b3a64014f581a75afd5b331f966b2969a421c06a
scratchmasterj21 <130957031+scratchmasterj21@users.noreply.github.com>
2024-03-28 07:36:55 +0000

    update

--- src/playground/render-gui.jsx
+++ src/playground/render-gui.jsx
@@ -8,7 +8,7 @@ import HashParserHOC from '../lib/hash-parser-hoc.jsx';
 import log from '../lib/log.js';
 
 const onClickLogo = () => {
-    window.location = 'https://scratch.mit.edu';
+    window.location = 'https://scratchmasterj21.github.io/felice-scratch/';
 };
 
 const handleTelemetryModalCancel = () => {
